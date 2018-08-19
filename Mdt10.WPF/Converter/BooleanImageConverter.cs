1476e4ddc8dc310e7c27d343bef5594b9a0d6d80
PhilippeTHEODORE <philippe.theodore@laposte.net>
2018-08-19 18:36:12 +0200

    Commit Icones

---
+++ Mdt10.WPF/Converter/BooleanImageConverter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace Mdt10.WPF.Converter
+{
+
+    // Custom class implements the IValueConverter interface.
+    public class BooleanConverter : IValueConverter
+    {
+
+        #region IValueConverter Members
+
+
+
+        // Define the Convert method to change a DateTime object to 
+        // a month string.
+        public object Convert(object value, Type targetType,
+            object parameter, CultureInfo language)
+        {
+            if ((bool)value)
+                return "OK";
+            else
+                return "KO";
+        }
+
+        // ConvertBack is not implemented for a OneWay binding.
+        public object ConvertBack(object value, Type targetType,
+            object parameter, CultureInfo language)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+
+
+
+
+}
